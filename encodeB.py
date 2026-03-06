58bab5da35268e14fdd05d0708c82f0efbe81fae
ashnoor25597 <ashnoor25597@iiitd.ac.in>
2026-03-06 23:17:01 +0530

    Implement encodeB function for instruction encoding

---
+++ encodeB.py
@@ -0,0 +1,13 @@
+def encodeB(instruction,reg1,reg2,branch_offset):
+    instr_info=instructions[instruction]
+    opcode=instr_info["opcode"]
+    func3=instr_info["func3"]
+    rs1=registers[reg1]
+    rs2=registers[reg2]
+    imm=toBinary(branch_offset,13)
+    imm12=imm[0]
+    imm10_5=imm[1:7]
+    imm4_1=imm[7:11]
+    imm11=imm[11]
+    encode=imm12+imm10_5+rs2+rs1+func3+imm4_1+imm11+opcode
+    return encode
