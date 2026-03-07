128797ee74857b5860ac0f6c47c19bcd6bcd1161
vijay25637 <vijay25637@iiitd.ac.in>
2026-03-07 15:18:30 +0530

    Update encodeU.py

--- encodeU.py
+++ encodeU.py
@@ -1,11 +1,17 @@
+#encode"U"
 def encodeU(inst_name,ops,line_no):
     info=instructions[inst_name]
 
     if len(ops)!=2:
         raise Exception("line"+str(line_no)+":wrong operand count")
+
     rd_name=ops[0].strip()
     imm_text=ops[1].strip()
-    rd=reg_bits(rd_name,line_no)
+
+    if rd_name not in registers:
+        raise Exception("line"+str(line_no)+":invalid register")
+
+    rd=registers[rd_name]
 
     try:
         imm=int(imm_text,0)
@@ -14,5 +20,7 @@ def encodeU(inst_name,ops,line_no):
 
     if not check_range(imm,32):
         raise Exception("line"+str(line_no)+":immediate out of range")
+
     imm_bits=toBinary(imm,32)[:20]
+
     return imm_bits+rd+info["opcode"]
