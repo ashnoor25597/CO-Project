abba0ebdc863aa9db8b2cc75f124e210df0dd292
srishti25629-star <srishti25629@iiitd.ac.in>
2026-03-09 20:33:41 +0530

    reading the assembly file (srishti)

--- Assembler.py
+++ Assembler.py
@@ -1,1 +1,20 @@
 import sys
+
+def read_file(filename=None):
+    lines=[]
+
+    if filename:   # if a filename is provided
+        with open(filename,"r") as file:
+            for line in file:
+                line=line.strip()
+                if line=="":
+                    continue
+                lines.append(line)
+    else:          # read from stdin (used by tester)
+        for line in sys.stdin:
+            line=line.strip()
+            if line=="":
+                continue
+            lines.append(line)
+
+    return lines
