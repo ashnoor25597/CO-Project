e622d2540eaa94628cadab77ec93e0d9fa11ea31
paavani25611-ship-it <paavani25611@iiitd.ac.in>
2026-03-09 20:52:33 +0530

    Ensuring that immediate is a number (Paavani)

--- Assembler.py
+++ Assembler.py
@@ -73,3 +73,32 @@ def checkrange(value,bits):
         return True
     else:
         return False
+    
+def checknumval(num):
+    
+    if len(num)==0:
+        return False
+    
+    if num[0]=="-":
+        num=num[1:]
+
+    if num.startswith ("0x") or num.startswith("0X"):
+        hexpart=num[2:]
+
+        if len(hexpart)==0:
+            return False
+        
+        for char in hexpart:
+            if not(char.isdigit() or char.lower() in "abcdef"):
+                return False
+        
+        return True
+    
+
+    for char in num:
+        if not char.isdigit():
+            return False
+        
+    return True
+
+def convertint(num):
