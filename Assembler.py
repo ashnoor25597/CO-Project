9574c4bfcc8323580a98b41afc8d55b618ab5496
paavani25611-ship-it <paavani25611@iiitd.ac.in>
2026-03-09 20:53:46 +0530

    Converting string to integer (Paavani)

--- Assembler.py
+++ Assembler.py
@@ -102,3 +102,10 @@ def checknumval(num):
     return True
 
 def convertint(num):
+    if num.startswith("0x") or num.startswith("0X") or num.startswith("-0x") or num.startswith("-0X"):
+        return int(num,16)    
+    
+    if num.startswith("-"):
+        return int(num)
+    
+    return int(num)
